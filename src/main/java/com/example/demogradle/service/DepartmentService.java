c8ff269791fe9fc6209c70d545117bf234a37a6d
Ashwini V <92977685+ashwiniv6@users.noreply.github.com>
2022-01-22 21:44:03 +0530

    Service

--- src/main/java/com/example/demogradle/service/DepartmentService.java
+++ src/main/java/com/example/demogradle/service/DepartmentService.java
@@ -1,6 +1,7 @@
 package com.example.demogradle.service;
 
 import com.example.demogradle.entity.Department;
+import com.example.demogradle.error.DepartmentNotFoundException;
 
 import java.util.List;
 
@@ -9,7 +10,7 @@ public interface DepartmentService {
 
    public List<Department> fetchDepartmentList();
 
-    public Department fetchDepartmentById(Long departmentId);
+    public Department fetchDepartmentById(Long departmentId) throws DepartmentNotFoundException;
 
    public  void deleteDepartmentById(Long departmentId);
 
--- src/main/java/com/example/demogradle/service/DepartmentServiceImpl.java
+++ src/main/java/com/example/demogradle/service/DepartmentServiceImpl.java
@@ -1,18 +1,22 @@
 package com.example.demogradle.service;
 
+
 import com.example.demogradle.entity.Department;
+import com.example.demogradle.error.DepartmentNotFoundException;
 import com.example.demogradle.repository.DepartmentRepository;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
 import java.util.List;
 import java.util.Objects;
+import java.util.Optional;
 
 @Service
 public class DepartmentServiceImpl implements DepartmentService{
 
     @Autowired
     private DepartmentRepository departmentRepository;
+
     @Override
     public Department saveDepartment(Department department) {
         return departmentRepository.save(department);
@@ -24,8 +28,15 @@ public class DepartmentServiceImpl implements DepartmentService{
     }
 
     @Override
-    public Department fetchDepartmentById(Long departmentId) {
-        return departmentRepository.findById(departmentId).get();
+    public Department fetchDepartmentById(Long departmentId) throws DepartmentNotFoundException {
+        Optional<Department> department =
+                departmentRepository.findById(departmentId);
+
+        if(!department.isPresent()) {
+            throw new DepartmentNotFoundException("Department Not Available");
+        }
+
+        return  department.get();
     }
 
     @Override
@@ -33,29 +44,26 @@ public class DepartmentServiceImpl implements DepartmentService{
         departmentRepository.deleteById(departmentId);
     }
 
-
-
     @Override
     public Department updateDepartment(Long departmentId, Department department) {
-        Department deptDB = departmentRepository.findById(departmentId).get();
+        Department depDB = departmentRepository.findById(departmentId).get();
 
-        //Null checks and Blank checks for all the fields
         if(Objects.nonNull(department.getDepartmentName()) &&
-        !"".equalsIgnoreCase(department.getDepartmentName())){
-            deptDB.setDepartmentName(department.getDepartmentName());
+                !"".equalsIgnoreCase(department.getDepartmentName())) {
+            depDB.setDepartmentName(department.getDepartmentName());
         }
 
         if(Objects.nonNull(department.getDepartmentCode()) &&
-                !"".equalsIgnoreCase(department.getDepartmentCode())){
-            deptDB.setDepartmentCode(department.getDepartmentCode());
+                !"".equalsIgnoreCase(department.getDepartmentCode())) {
+            depDB.setDepartmentCode(department.getDepartmentCode());
         }
 
         if(Objects.nonNull(department.getDepartmentAddress()) &&
-                !"".equalsIgnoreCase(department.getDepartmentAddress())){
-            deptDB.setDepartmentAddress(department.getDepartmentAddress());
+                !"".equalsIgnoreCase(department.getDepartmentAddress())) {
+            depDB.setDepartmentAddress(department.getDepartmentAddress());
         }
 
-        return departmentRepository.save(deptDB);
+        return departmentRepository.save(depDB);
     }
 
     @Override
