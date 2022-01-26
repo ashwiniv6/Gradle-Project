0defb2b62ba4b80ce45756a6adbe2643705c4b64
Ashwini V <92977685+ashwiniv6@users.noreply.github.com>
2022-01-26 12:54:38 +0530

    DTO

---
+++ src/main/java/com/example/demogradle/dto/UserDto.java
@@ -0,0 +1,13 @@
+package com.example.demogradle.dto;
+
+import lombok.Data;
+
+@Data
+public class UserDto {
+    private Long departmentId;
+    private String departmentName;
+    private String departmentAddress;
+    private String departmentCode;
+
+
+}
